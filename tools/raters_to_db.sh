9d1ed55be032f3d6d3e664271cd2f13f7c68a9c5
Emil Trenckner Jessen <emiltrencknerjessen@Emils-MacBook-Pro.local>
2023-01-02 15:19:33 +0100

    trying to add spacy to db, to begin manual resolvement of annotation disagreements for multi

--- tools/raters_to_db.sh
+++ tools/raters_to_db.sh
@@ -1,5 +1,26 @@
-for i in {1..10}
+while getopts p:d:o: flag
 do
-    echo "Exporting rater_$i data to Prodigy database ..."
-    prodigy db-in rater_"$i" ./data/prodigy_exports/prodigy"$i"_db_exports/NER_annotator"$i".jsonl
+    case "${flag}" in
+        p) parent_dir=${OPTARG};;
+        d) dir=${OPTARG};;
+        o) original=${OPTARG};;
+    esac
 done
+
+if [[ $original = 1 ]]; 
+then
+    for i in {1..10}
+    do
+        echo "Exporting rater_$i data to Prodigy database ..."
+        prodigy db-in rater_"$i" ./data/prodigy_exports/prodigy"$i"_db_exports/NER_annotator"$i".jsonl
+    done
+fi
+
+if [[ $original = 0 ]];
+then
+    for i in {1..10}
+    do
+        echo "Exporting rater_$i data to Prodigy database from ./data/$parent_dir/$dir/* ..."
+        prodigy db-in rater_"$i" ./data/$parent_dir/$dir/rater_$i/data.spacy
+    done
+fi
