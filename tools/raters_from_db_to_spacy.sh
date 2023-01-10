8937a147e826a2dcb0393799da3e089c058923d5
Emil Trenckner Jessen <emiltrencknerjessen@Emils-MacBook-Pro.local>
2023-01-10 16:09:41 +0100

    merge conflict fix

--- tools/raters_from_db_to_spacy.sh
+++ tools/raters_from_db_to_spacy.sh
@@ -1,7 +1,7 @@
 for i in {1..10}
 do
     echo "Exporting data from data base for rater_$i to  ..."
-    prodigy data-to-spacy data/full/unprocessed/rater_$i/ --ner rater_$i --lang "da" --eval-split 0
+    prodigy data-to-spacy data/full/unprocessed/rater_$i/ --ner rater_$i --lang "da" --eval-split 0 #0.2
     #rm -r data/full/unprocessed/rater_$i/labels
     #rm data/full/unprocessed/rater_$i/config.cfg
 done
